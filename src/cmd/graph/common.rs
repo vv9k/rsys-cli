5ec98abfa3884eaf0863b868900aa7203946b193
wojciechkepka <wojtek.kepka@protonmail.com>
2020-10-05 16:55:14 +0200

    [graph] Add new utility Spans funcs

--- src/cmd/graph/common.rs
+++ src/cmd/graph/common.rs
@@ -10,8 +10,25 @@ use rsys::linux;
 use tui::{
     backend::Backend,
     layout::{Constraint, Layout, Rect},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
     Frame,
 };
+
+type KvSpan<'a> = [Span<'a>; 2];
+
+pub(crate) fn kv_span<'a, T: Into<String>>(k: T, v: T, color: Color, bold: bool) -> KvSpan<'a> {
+    let val = if bold {
+        Span::styled(v.into(), Style::default().fg(color).add_modifier(Modifier::BOLD))
+    } else {
+        Span::styled(v.into(), Style::default().fg(color))
+    };
+    [Span::raw(k.into()), val]
+}
+
+pub(crate) fn spans_from<'a>(kvspans: Vec<KvSpan<'a>>) -> Spans<'a> {
+    Spans::from(kvspans.concat())
+}
 
 /// Trait grouping all graph widgets together providing functionality
 /// like graph_loop.
