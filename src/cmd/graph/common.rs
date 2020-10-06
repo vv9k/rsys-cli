d4ccc0592edf1c905dd9da41b886e8b9dbaa6862
wojciechkepka <wojtek.kepka@protonmail.com>
2020-10-06 07:46:44 +0200

    [graph] Add common RxTx struct

--- src/cmd/graph/common.rs
+++ src/cmd/graph/common.rs
@@ -5,9 +5,14 @@ use super::{
     interface::IfaceMonitor,
     storage::StorageMonitor,
 };
+use crate::util::conv_fb;
 use anyhow::Result;
 use rsys::linux;
-use std::time::Instant;
+use std::{
+    ops::AddAssign,
+    fmt::Debug,
+    time::Instant
+};
 use tui::{
     backend::Backend,
     layout::{Constraint, Layout, Rect},
@@ -224,5 +229,34 @@ impl Monitor {
 
     pub fn x(&self) -> [f64; 2] {
         self.x_axis
+    }
+}
+
+#[derive(Default, Debug)]
+pub(crate) struct RxTx<T: Default + Debug + AddAssign>(pub (T, T));
+impl<T: Default + Debug + AddAssign> RxTx<T> {
+    pub(crate) fn rx(&self) -> &T {
+        &self.0.0
+    }
+    pub(crate) fn tx(&self) -> &T {
+        &self.0.1
+    }
+    pub(crate) fn inc(&mut self, r: T, t: T) {
+        self.0.0 += r;
+        self.0.1 += t;
+    }
+}
+impl RxTx<f64> {
+    pub(crate) fn rx_speed_str(&self) -> String {
+        format!("{}/s", conv_fb(*self.rx()))
+    }
+    pub(crate) fn tx_speed_str(&self) -> String {
+        format!("{}/s", conv_fb(*self.rx()))
+    }
+    pub(crate) fn rx_bytes_str(&self) -> String {
+        conv_fb(*self.rx())
+    }
+    pub(crate) fn tx_bytes_str(&self) -> String {
+        conv_fb(*self.tx())
     }
 }
