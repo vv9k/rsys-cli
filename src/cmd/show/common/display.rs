9cb1fbda2bc6f33db4dfffb7dcc9d7198f56f38e
wojciechkepka <wojtek.kepka@protonmail.com>
2020-10-08 14:53:55 +0200

    [show/common] Add err_tab for displaying error msgs

--- src/cmd/show/common/display.rs
+++ src/cmd/show/common/display.rs
@@ -1,6 +1,8 @@
+use std::borrow::Cow;
 use tui::{
     style::{Color, Modifier, Style},
     text::{Span, Spans},
+    widgets::{Block, Borders, Paragraph},
 };
 
 type KvSpan<'a> = [Span<'a>; 2];
@@ -16,4 +18,17 @@ pub fn kv_span<'a, T: Into<String>>(k: T, v: T, color: Color, bold: bool) -> KvS
 
 pub fn spans_from<'a>(kvspans: Vec<KvSpan<'a>>) -> Spans<'a> {
     Spans::from(kvspans.concat())
+}
+
+pub fn err_tab<'a, T: Into<Cow<'a, str>>>(message: T) -> Paragraph<'a> {
+    Paragraph::new(Span::styled(
+        message.into(),
+        Style::default().fg(Color::Red).add_modifier(Modifier::ITALIC),
+    ))
+    .block(
+        Block::default()
+            .borders(Borders::ALL)
+            .title("Info")
+            .style(Style::default().add_modifier(Modifier::BOLD)),
+    )
 }
